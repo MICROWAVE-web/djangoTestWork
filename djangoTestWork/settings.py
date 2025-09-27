ca9cd88cbcd89f5b701599d803f8f41e06e807da
kovanov <playalexei@yandex.ru>
2025-09-27 20:10:15 +0700

    Final Commit

--- djangoTestWork/settings.py
+++ djangoTestWork/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS").split(",")
 
 # Application definition
 
